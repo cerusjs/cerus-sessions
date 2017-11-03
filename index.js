db976ab3cad329d74fa3543964536ded5f8db287
JortvD <jortvandriel@hotmail.com>
2017-11-03 13:01:05 +0100

    Added the package.json

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = function() {
+	var self = {};
+
+	var package = require("./package.json");
+	
+	self.name = package["name"];
+	self.version = package["version"];
+	self.dependencies = [
+		"cerus",
+		"cerus-settings",
+		"cerus-router",
+		"cerus-uuid"
+	];
+
+	var sessions;
+
+	self.init_ = function(cerus) {
+		sessions = require("./lib/sessions")();
+
+		cerus.router().use()
+		.then(function(req, res) {
+			var sessions_ = sessions.get(res, req);
+			
+			res.sessions = function() {
+				return sessions_;
+			}
+		});
+	}
+
+	return self;
+}
