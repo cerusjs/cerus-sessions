213418b7efd459286a15ae68089eee61fc50defb
JortvD <jortvandriel@hotmail.com>
2017-11-03 13:17:19 +0100

    Moved the settings to the init function

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ module.exports = function() {
 	var sessions;
 
 	self.init_ = function(cerus) {
+		cerus.settings().setting("sessions.secret", "THIS_IS_A_SECRET");
+		cerus.settings().setting("sessions.cookie.http", true);
+		cerus.settings().setting("sessions.cookie.age", undefined);
+		cerus.settings().setting("sessions.cookie.path", "/");
+		cerus.settings().setting("sessions.cookie.same", true);
+		cerus.settings().setting("sessions.cookie.secure", false);
+		cerus.settings().setting("sessions.cookie.name", "_sessions");
+		cerus.settings().setting("sessions.cookie.proxy", false);
+		cerus.settings().setting("sessions.resave", false);
+		cerus.settings().setting("sessions.rolling", false);
+		cerus.settings().setting("sessions.safe", true);
+		cerus.settings().setting("sessions.unset", false);
+
 		sessions = require("./lib/sessions")(cerus);
 
 		cerus.router().use()
