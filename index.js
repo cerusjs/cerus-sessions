663967e197849b02b489e8ec143e955a064f71c7
JortvD <jortvandriel@hotmail.com>
2018-03-22 17:38:47 +0100

    Stripped the settings module away

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = function() {
 	plugin.version = package["version"];
 	plugin.dependencies = [
 		"cerus",
-		"cerus-settings",
 		"cerus-router",
 		"cerus-uuid"
 	];
@@ -14,19 +13,6 @@ module.exports = function() {
 	var sessions;
 
 	plugin.init_ = function(cerus) {
-		cerus.settings().setting("sessions.secret", "THIS_IS_A_SECRET");
-		cerus.settings().setting("sessions.cookie.http", true);
-		cerus.settings().setting("sessions.cookie.age", undefined);
-		cerus.settings().setting("sessions.cookie.path", "/");
-		cerus.settings().setting("sessions.cookie.same", true);
-		cerus.settings().setting("sessions.cookie.secure", false);
-		cerus.settings().setting("sessions.cookie.name", "_sessions");
-		cerus.settings().setting("sessions.cookie.proxy", false);
-		cerus.settings().setting("sessions.resave", false);
-		cerus.settings().setting("sessions.rolling", false);
-		cerus.settings().setting("sessions.safe", true);
-		cerus.settings().setting("sessions.unset", false);
-
 		sessions = new (require("./lib/sessions"))(cerus);
 
 		// TODO: Needs to be updated
